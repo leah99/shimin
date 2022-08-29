17f274ad5a38fec2a00abf7800f8e81c50d3bf82
leah99 <leah.low@newwealth.com.sg>
2022-08-29 18:17:32 +0800

    feat(Home): move home component to pages and add css for tablet view

--- shimin/src/Pages/Home/Home.js
+++ shimin/src/Pages/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import linkedInIcon from "../../assets/linkedIn.svg";
-import emailIcon from "../../assets/email.svg";
-import {ReactComponent as Girl} from "../../assets/girl-working.svg"
-
+import { TextSwapping } from '../../components';
+import {ReactComponent as LinkedIn} from "../../assets/linkedIn.svg"
+import {ReactComponent as Email} from "../../assets/email.svg"
+import {ReactComponent as WomanWorking} from "../../assets/woman-working.svg"
 import './Home.scss';
 
-const Home = ({ titleGreeting, titlePronoun, titleColor, subtitle, linkedInLink, linkedInIcon, emailAddress, emailIcon}) => {
+const Home = ({ titleGreeting, titlePronoun, titleColor, subtitle, linkedInLink, emailAddress}) => {
     return (
         <div className="home">
             <div className="home__container-left">
@@ -15,44 +14,38 @@ const Home = ({ titleGreeting, titlePronoun, titleColor, subtitle, linkedInLink,
                     <span className="home__container-left__title__greeting">{titleGreeting}</span>
                     <div className="home__container-left__title__container">
                         <span className="home__container-left__title__container__pronoun">{titlePronoun}</span>
-                        <span className="home__container-left__title__container__color">{titleColor}</span>
+                        <span className="home__container-left__title__container__color"><TextSwapping text={titleColor} /></span>
                     </div>
                 </div>
                 <span className="home__container-left__subtitle">{subtitle}</span>
                 <div className="home__container-left__icons">
-                    <a href={linkedInLink}><img className="home__container-left__icons__linkedIn" src={linkedInIcon} alt=''/></a>
-                    <a href={emailAddress}><img className="home__container-left__icons__email" src={emailIcon} alt=''/></a>
+                    <a href={linkedInLink}><LinkedIn className="home__container-left__icons__linkedIn" /></a>
+                    <a href={emailAddress}><Email className="home__container-left__icons__email" /></a>
                 </div>
             </div>
-            <Girl className="home__container-right" />
+            <WomanWorking className="home__container-right" />
         </div>
     )
 }
 
 Home.propTypes = {
     /** Title -- greeting */
-    titleGreeting: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    titleGreeting: PropTypes.string,
 
     /** Title -- pronoun */
-    titlePronoun: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    titlePronoun: PropTypes.string,
 
     /** Title -- color */
-    titleColor: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    titleColor: PropTypes.string,
 
     /** Subtitle -- text under title */
-    subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    subtitle: PropTypes.string,
 
     /** LinkedIn link */
-    linkedInLink: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-
-    /** LinkedIn icon */
-    linkedInIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node, 'Photos']),
+    linkedInLink: PropTypes.string,
 
     /** Email link */
-    emailAddress: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-
-    /** Email icon */
-    emailIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node, 'Photos']),
+    emailAddress: PropTypes.string,
 }
 
 Home.defaultProps = {
@@ -61,9 +54,7 @@ Home.defaultProps = {
     titleColor: "Shi Min",
     subtitle: "I am a fresh graduate from Nanyang Technological University with a degree of Bachelor of Engineering (Computer Science). My specialisations are Artificial Intelligence and Cyber Security.",
     linkedInLink: "https://www.linkedin.com/in/shi-min-low-8a09591bb",
-    linkedInIcon: linkedInIcon,
     emailAddress: "mailto:shimin.loww@gmail.com",
-    emailIcon: emailIcon,
 };
 
 export default Home
