0207a3bbb0d55eb9c7368784546ecf38a3761bd0
leah99 <leah.low@newwealth.com.sg>
2022-08-30 00:38:56 +0800

    feat(Paragraph): paragraph component

--- shimin/src/components/Paragraph/Paragraph.js
+++ shimin/src/components/Paragraph/Paragraph.js
@@ -38,11 +38,11 @@ Paragraph.defaultProps = {
 };
 
 /** Example usage of Paragraph */
-{/* <Paragraph
-    customClass=""
-    paragraph={["Hey there! My name is Shi Min but you can call me Leah! I am currently a fresh graduate from Nanyang Technology University studying Computer Science. My interest lies with front-end but I am also open to learning some UI/UX designing.", 
-                "I love to clean and organise during my free time. My current favourite tea is ginger garden by Pryce tea."]}
-    coloredText="Leah"
-/> */}
+// {/* <Paragraph
+//     customClass=""
+//     paragraph={["Hey there! My name is Shi Min but you can call me Leah! I am currently a fresh graduate from Nanyang Technology University studying Computer Science. My interest lies with front-end but I am also open to learning some UI/UX designing.", 
+//                 "I love to clean and organise during my free time. My current favourite tea is ginger garden by Pryce tea."]}
+//     coloredText="Leah"
+// /> */}
 
 export default Paragraph
