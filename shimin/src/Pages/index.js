e0e099fc8379798b353a7f0e58e9d2cda4ecb1ff
leah99 <leah.low@newwealth.com.sg>
2022-08-30 00:40:02 +0800

    feat(index): index file

---
+++ shimin/src/Pages/index.js
@@ -0,0 +1,13 @@
+import Header from '../Pages/Header';
+import Home from '../Pages/Home';
+import Projects from '../Pages/Projects';
+import Contact from '../Pages/Contact';
+import Footer from '../Pages/Footer';
+
+export {
+    Header,
+    Home,
+    Projects,
+    Contact,
+    Footer,
+};
---
+++ shimin/src/components/index.js
@@ -0,0 +1,32 @@
+import Header from '../Pages/Header';
+import Home from '../Pages/Home';
+import About from '../Pages/About';
+import Projects from '../Pages/Projects';
+import Contact from '../Pages/Contact';
+import Footer from '../Pages/Footer';
+
+import Tab from './Tab';
+import Content from './Content';
+import BounceEffectLink from './BounceEffectLink';
+import GrowingBackground from './GrowingBackground';
+import Highlight from './Highlight';
+import TextSwapping from './TextSwapping';
+import Card from './Card';
+import Paragraph from './Paragraph';
+
+export {
+    Header,
+    Home,
+    About,
+    Projects,
+    Contact,
+    Footer,
+    Tab,
+    Content,
+    BounceEffectLink,
+    GrowingBackground,
+    Highlight,
+    TextSwapping,
+    Card,
+    Paragraph,
+};
