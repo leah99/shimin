662ad2a722156486a1658f44c39c52a21ea8bf9c
Shi Min <shimin.loww@gmail.com>
2022-09-15 16:13:11 +0800

    feat(Contact): add animation on scroll

--- shimin/src/Pages/Contact/Contact.js
+++ shimin/src/Pages/Contact/Contact.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import {Highlight} from "../../components"
-import {ReactComponent as LinkedIn} from "../../assets/linkedIn.svg"
-import {ReactComponent as Email} from "../../assets/email.svg"
-
+import {Highlight} from "../../components";
+import {ReactComponent as LinkedIn} from "../../assets/linkedIn.svg";
+import {ReactComponent as Email} from "../../assets/email.svg";
+import AOS from "aos";
 import "./Contact.scss";
   
 const Contact = ({header, paragraph, linkedInLink, emailAddress}) => {
-	return (
-        <div className="contact">
+	useEffect(() => {
+        AOS.init({duration : 500});
+		AOS.refresh();
+    }, [])
+
+    return (
+        <div className="contact" data-aos="zoom-in-down">
             <div className="contact__header"><Highlight text={header} /></div>
             <div className="contact__paragraph">
                 {paragraph.split('\n').map(str => <p key={str}>{str}</p>)}
